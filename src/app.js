60c961ac31836fa27a94667065996003c00b8582
EmeryP <36278398+EmeryP@users.noreply.github.com>
2018-12-01 14:43:08 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,12 +6,13 @@ import cors from 'cors';
 
 const app = express();
 
+app.use(cors())
+
 app.use(express.json());
 
 app.use(router)
 app.use(error)
 app.use(notFound)
-app.use(cors)
 
 let isRunning = false;
 
