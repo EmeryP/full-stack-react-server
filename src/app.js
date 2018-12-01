cf4d4933b4849a6c0d8ebe327382f02a8c281777
emeryparks <emeryparks09@hotmail.com>
2018-12-01 14:38:04 -0800

    added cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import router from './api/routes.js'
 import error from './public/error.js';
 import notFound from './public/404.js';
+import cors from 'cors';
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(router)
 app.use(error)
 app.use(notFound)
+app.use(cors)
 
 let isRunning = false;
 
